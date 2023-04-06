30db25f8f16badcdb030ae26cf980cbfb08f4aae
yuneunsuk <familyman80@naver.com>
2023-04-06 18:57:34 +0900

    babel

--- next.config.js
+++ next.config.js
@@ -12,25 +12,6 @@ const nextConfig = {
     domains: ["dev-gopizza-store-service.s3.ap-northeast-2.amazonaws.com"],
     unoptimized: true,
   },
-  compiler: {
-    emotion: {
-      // default is true. It will be disabled when build type is production.
-      sourceMap: true,
-      // default is 'dev-only'.
-      autoLabel: "always",
-      // default is '[local]'.
-      // Allowed values: `[local]` `[filename]` and `[dirname]`
-      // This option only works when autoLabel is set to 'dev-only' or 'always'.
-      // It allows you to define the format of the resulting label.
-      // The format is defined via string where variable parts are enclosed in square brackets [].
-      // For example labelFormat: "my-classname--[local]", where [local] will be replaced with the name of the variable the result is assigned to.
-      labelFormat: "[local]",
-      // default is undefined.
-      // This option allows you to tell the compiler what imports it should
-      // look at to determine what it should transform so if you re-export
-      // Emotion's exports, you can still use transforms.
-    },
-  },
 };
 
 module.exports = nextConfig;

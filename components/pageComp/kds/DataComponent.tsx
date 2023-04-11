54f1288948ec76fce72bb41ef0a33ee28f9f9796
yuneunsuk <familyman80@naver.com>
2023-04-11 16:10:35 +0900

    empty

--- components/pageComp/kds/DataComponent.tsx
+++ components/pageComp/kds/DataComponent.tsx
@@ -1,3 +1,4 @@
+import { EmptyView } from "ComponentsFarm/elements/EmptyView";
 import { IOrderListRes } from "InterfaceFarm/Kds";
 import React from "react";
 
@@ -21,74 +22,80 @@ const DataComponent = ({ data }: { data: IOrderListRes }) => {
   return (
     <div className="wrap_real_order">
       <h3>실시간 주문 현황</h3>
-      {data.list.map((item) => (
-        <div className="box">
-          <div className="head_item">
-            <div className="item_receipt">
-              <div className="box_status_info">
-                <div className="num_order">
-                  {item.receipt_number}
-                  <span className="current_status">
-                    {" "}
-                    (배달 / {getStatusText(item.process_status)})
-                  </span>{" "}
-                </div>
-                <div className="txt_info">
-                  <div className="kind">
-                    <span className="txt_date">{item.ordered_at}</span>
+      {data.list.length === 0 ? (
+        <div className="box" style={{ position: "relative", minHeight: 130 }}>
+          <EmptyView full>조회된 데이터가 없습니다.</EmptyView>
+        </div>
+      ) : (
+        data.list.map((item) => (
+          <div className="box">
+            <div className="head_item">
+              <div className="item_receipt">
+                <div className="box_status_info">
+                  <div className="num_order">
+                    {item.receipt_number}
+                    <span className="current_status">
+                      {" "}
+                      (배달 / {getStatusText(item.process_status)})
+                    </span>{" "}
+                  </div>
+                  <div className="txt_info">
+                    <div className="kind">
+                      <span className="txt_date">{item.ordered_at}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="cont_item">
-            <table>
-              <colgroup>
-                <col width="70%" />
-                <col width="15%" />
-                <col width="15%" />
-              </colgroup>
-              <thead>
-                <tr>
-                  <th scope="col">메뉴이름</th>
-                  <th scope="col">수량</th>
-                  <th scope="col">상태</th>
-                </tr>
-              </thead>
-              <tbody>
-                {item.item_list
-                  .map((itemList) => [
-                    <tr key={itemList.receipt_item_idx}>
-                      <td>
-                        <span>{itemList.product_name}</span>
-                      </td>
-                      <td>
-                        <span>{itemList.quantity}</span>
-                      </td>
-                      <td>
-                        <span>{getStatusText(itemList.process_status)}</span>
-                      </td>
-                    </tr>,
-                    itemList.option_list.map((option) => (
-                      <tr key={option.receipt_item_idx}>
-                        <td style={{ paddingLeft: "30px" }}>
-                          <span>· {option.product_name}</span>
+            <div className="cont_item">
+              <table>
+                <colgroup>
+                  <col width="70%" />
+                  <col width="15%" />
+                  <col width="15%" />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th scope="col">메뉴이름</th>
+                    <th scope="col">수량</th>
+                    <th scope="col">상태</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {item.item_list
+                    .map((itemList) => [
+                      <tr key={itemList.receipt_item_idx}>
+                        <td>
+                          <span>{itemList.product_name}</span>
                         </td>
                         <td>
-                          <span>{option.quantity}</span>
+                          <span>{itemList.quantity}</span>
                         </td>
                         <td>
-                          <span>{getStatusText(option.process_status)}</span>
+                          <span>{getStatusText(itemList.process_status)}</span>
                         </td>
-                      </tr>
-                    )),
-                  ])
-                  .reduce((acc, curr) => acc.concat(curr), [])}
-              </tbody>
-            </table>
+                      </tr>,
+                      itemList.option_list.map((option) => (
+                        <tr key={option.receipt_item_idx}>
+                          <td style={{ paddingLeft: "30px" }}>
+                            <span>· {option.product_name}</span>
+                          </td>
+                          <td>
+                            <span>{option.quantity}</span>
+                          </td>
+                          <td>
+                            <span>{getStatusText(option.process_status)}</span>
+                          </td>
+                        </tr>
+                      )),
+                    ])
+                    .reduce((acc, curr) => acc.concat(curr), [])}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
